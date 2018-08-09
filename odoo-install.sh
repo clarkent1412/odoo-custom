e167f4d4bfe0cf6b6594473b21f3ad9234d8d68f
clarkent1412 <40132471+clarkent1412@users.noreply.github.com>
2018-08-09 22:30:42 +0700

    Create odoo-install.sh

---
+++ odoo-install.sh
@@ -0,0 +1,4 @@
+sudo apt update && sudo apt upgrade -y 
+sudo apt install git python3-pip build-essential python3-dev libxslt-dev libzip-dev libldap2-dev libsasl2-dev node-less -y
+
+https://linuxize.com/post/install-odoo-11-on-ubuntu-16-04/
